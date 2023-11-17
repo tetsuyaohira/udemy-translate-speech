d857f5f323c1767fe3f1b83e43cb80068ab35cca
tetsuyaohira <tetsuya.ohira@icloud.com>
2023-11-17 20:40:33 +0900

    多言語対応

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -4,28 +4,23 @@ type Language = {
 }
 
 export const LANGUAGES: Language[] = [
-  {
-    translate: 'de',
-    speak: 'de-DE',
-  },
-  {
-    translate: 'fr',
-    speak: 'fr-FR',
-  },
-  {
-    translate: 'it',
-    speak: 'it-IT',
-  },
-  {
-    translate: 'ja',
-    speak: 'ja-JP',
-  },
-  {
-    translate: 'ko',
-    speak: 'ko-KR',
-  },
-  {
-    translate: 'ru',
-    speak: 'ru-RU',
-  },
+  { translate: 'de', speak: 'de-DE' }, // ドイツ語（ドイツ）
+  { translate: 'fr', speak: 'fr-FR' }, // フランス語（フランス）
+  { translate: 'fr', speak: 'fr-CA' }, // フランス語（カナダ）
+  { translate: 'it', speak: 'it-IT' }, // イタリア語（イタリア）
+  { translate: 'ja', speak: 'ja-JP' }, // 日本語（日本）
+  { translate: 'ko', speak: 'ko-KR' }, // 韓国語（韓国）
+  { translate: 'ru', speak: 'ru-RU' }, // ロシア語（ロシア）
+  { translate: 'es', speak: 'es-ES' }, // スペイン語（スペイン）
+  { translate: 'tr', speak: 'tr-TR' }, // トルコ語（トルコ）
+  { translate: 'uk', speak: 'uk-UA' }, // ウクライナ語（ウクライナ）
+  { translate: 'zh-CN', speak: 'zh-CN' }, // 中国語（簡体字）
+  { translate: 'zh-TW', speak: 'zh-TW' }, // 中国語（繁体字）
+  { translate: 'hi', speak: 'hi-IN' }, // ヒンディー語
+  { translate: 'ar', speak: 'ar-SA' }, // アラビア語
+  { translate: 'pt', speak: 'pt-BR' }, // ポルトガル語（ブラジル）
+  { translate: 'pt', speak: 'pt-PT' }, // ポルトガル語（ポルトガル）
+  { translate: 'bn', speak: 'bn-BD' }, // ベンガル語（バングラデシュ）
+  { translate: 'fil', speak: 'fil-PH' }, // フィリピン語
+  { translate: 'id', speak: 'id-ID' }, // インドネシア語
 ]
